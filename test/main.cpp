5d2fabebab3021679e9e10d69db970c4688227e8
nikolaAV <Alexander.Nikolaenko@gmail.com>
2022-12-25 13:50:45 +0200

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+/*
+    g++ main.cpp -std=c++14 -o exe -g
+    g++ main.cpp -o exe
+   // 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
+*/
+
+// #define NDEBUG
+
+#include <cassert>
+#include <iostream>
+
+int main()
+{
+    assert(!"Hello, Nikola");
+    std::cout << "Hello, Nikola" << std::endl;
+}
